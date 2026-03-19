e9fa12ed7f2c8fa81eace6edc3753b191e0ed6c0
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2026-03-19 12:17:11 -0300

    feat: update shadcn theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,7 @@ const App = () => {
     <div>
       <h1 className="my-2 p-5 text-red-500">Welcome to my React App!</h1>
       <p>This is a simple React application.</p>
-      <Button
-        className="bg-black text-white hover:bg-blue-600"
-        variant="outline"
-      >
-        Click Me
-      </Button>
+      <Button>Click-me</Button>
     </div>
   )
 }
