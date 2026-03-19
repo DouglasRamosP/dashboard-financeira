7e294e45dfff88872ad089538e8bf46dec8206f6
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2026-03-18 22:52:03 -0300

    add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <h1 className="my-2 p-5 text-red-500">Welcome to my React App!</h1>
+      <p>This is a simple React application.</p>
+      <p>
+        You can add more components and functionality as needed. This is just a
+      </p>
+    </div>
+  )
 }
 
 export default App
