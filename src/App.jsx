33b66aa4154e7b2a4952a75416e1f0ce82373bae
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2026-03-19 11:17:08 -0300

    chore: add shadcn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
   return (
     <div>
       <h1 className="my-2 p-5 text-red-500">Welcome to my React App!</h1>
       <p>This is a simple React application.</p>
-      <p>
-        You can add more components and functionality as needed. This is just a
-      </p>
+      <Button
+        className="bg-black text-white hover:bg-blue-600"
+        variant="outline"
+      >
+        Click Me
+      </Button>
     </div>
   )
 }
